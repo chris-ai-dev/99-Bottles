3901250807ac1675c00b6acf48e657db9475b609
Chris <chrisvolar@gmail.com>
2022-05-27 18:23:13 -0500

    finish bot

--- 99_bottles.js
+++ 99_bottles.js
@@ -14,7 +14,7 @@ function bottleSong() {
     console.log(bottles.toString(), "bottles of beer on the wall,", bottles.toString(), "bottles of beer. \nTake one down and pass it around,",oneLess.toString(), "bottles of beer on the wall.");
     
     if(bottles == 2 ) {
-      console.log("Take one down and pass it around, 1 bottle of beer on the wall. \n1 bottle of beer on the wall, 1 bottle of beer. \nTake one down and pass it around, no more bottles of beer on the wall. \nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.");
+      console.log("1 bottle of beer on the wall, 1 bottle of beer. \nTake one down and pass it around, no more bottles of beer on the wall. \nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.");
       break;
     }
     
--- 99_bottles.py
+++ 99_bottles.py
@@ -8,7 +8,7 @@ def bottle_song():
 		print(f"{bottles} bottles of beer on the wall, {bottles} bottles of beer.\nTake one down and pass it around, {bottles-1}  bottles of beer on the wall.")
 		bottles -= 1
 		if bottles == 1:
-			print(f"1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.")
+			print(f"Take one down and pass it around, no more bottles of beer on the wall.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.")
 			break
 
 
