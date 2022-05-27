3d2054fd519864b1b4bfcec2667ea6ff4f404c12
Chris <chrisvolar@gmail.com>
2022-05-27 17:51:34 -0500

    finish both

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,18 @@
 def bottle_song():
 	# write your code here!
+	# name = input("what is your name?")
+	# print("your name is " + name)
+	
+	bottles = 99
+	while bottles > 0:
+		print(f"{bottles} bottles of beer on the wall, {bottles} bottles of beer.\nTake one down and pass it around, {bottles-1}  bottles of beer on the wall.")
+		bottles -= 1
+		if bottles == 1:
+			print(f"1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.")
+			break
+
+
+
 
 
 bottle_song()
